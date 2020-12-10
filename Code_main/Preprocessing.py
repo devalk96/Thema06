54113ee59657480e6caa9b0462b6af091edf9576
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2020-12-10 19:23:34 +0100

    Fasta_processing.py almost finished.

--- Code_main/Preprocessing.py
+++ Code_main/Preprocessing.py
@@ -13,6 +13,7 @@ import os
 import sys
 import glob
 
+
 class Preprocessing:
     """
     Preporcessing steps before creating the count file
@@ -32,17 +33,12 @@ class Preprocessing:
         return 0
 
     def process(self, filename, filepath):
-        os.system(f"java -jar {self.picard} SortSam I={filepath} "
-                  f"O={self.output_dir}/output/sortedbam/{filename}.bam SO=queryname")
-        os.system(f"java -jar {self.picard} AddOrReplaceReadGroups INPUT={filepath} "
-                  f"OUTPUT={self.output_dir}/output/readgroup/{filename}.bam "
-                  f"LB={filename} PU={filename} SM={filename} PL={self.platform} "
-                  f"CREATE_INDEX=TRUE")
-        os.system(f"java -jar {self.picard} FixMateInformation "
-                  f"INPUT={self.output_dir}/Preprocessing/addOrReplace/{filename}.bam")
-        os.system(f"java -jar {self.picard} MergeSamFiles INPUT={self.output_dir}/Preprocessing/addOrReplace/{filename}.bam "
-                  f"OUTPUT={self.output_dir}Preprocessing/mergeSam/{filename}.bam CREATE_INDEX=true USE_THREADING=true")
-        os.system(f"java -jar {self.picard}")
+        os.system(f"java -jar {self.picard} SortSam I={filepath} O={self.output_dir}/output/sortedBam/{filename}.bam SO=queryname")
+        os.system(f"java -jar {self.picard} AddOrReplaceReadGroups INPUT={filepath} OUTPUT={self.output_dir}/output/addOrReplace/{filename}.bam LB={filename} PU={filename} SM={filename} PL={self.platform} CREATE_INDEX=TRUE")
+        os.system(f"java -jar {self.picard} FixMateInformation INPUT={self.output_dir}/Preprocessing/addOrReplace/{filename}.bam")
+        os.system(f"java -jar {self.picard} MergeSamFiles INPUT={self.output_dir}/Preprocessing/addOrReplace/{filename}.bam OUTPUT={self.output_dir}Preprocessing/mergeSam/{filename}.bam CREATE_INDEX=true USE_THREADING=true")
+        os.system(f"java -jar {self.picard} MarkDuplicates INPUT={self.output_dir}/Preprocessing/mergeSam/{filename}.bam OUTPUT={self.output_dir}/Preprocessing/markDuplicates/{filename}.bam CREATE_INDEX=true METRICS_FILE={self.output_dir}/Preprocessing/markDuplicates/{filename}.metrics.log")
+        os.system(f"samtools sort -n {self.output_dir}/Preprocessing/markDuplicates/{filename}.bam -o {self.output_dir}/Preprocessing/markDuplicates/{filename}_sorted.bam")
 
 
 def main():
