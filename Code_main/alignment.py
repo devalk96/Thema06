1f257ac861acec61172a2c6cf4e5c9c452bf1c12
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2020-12-16 10:48:05 +0100

    Aligments.

--- Code_main/alignment.py
+++ Code_main/alignment.py
@@ -5,6 +5,8 @@ Script to map the reads.
 """
 import os
 import glob
+import sys
+import argparse
 
 
 class Alignment:
@@ -22,7 +24,43 @@ class Alignment:
         Map the files and save them as filename_aligned.sam
         """
         os.chdir(self.directory)
-        data = glob.glob("*.fa") + glob.glob("*.fasta")
-        for file in data:
-            print(f"{self.tool_path} -a {self.refseq} {file} > {self.output_path}/{file.split('.')[0]}_aligned.sam")
-            # os.system(f"{self.tool_path} -a {self.refseq} {file} > {file.split('.')[-1]}_aligned.sam")
+        for filename in os.listdir():
+            for file2 in os.listdir(filename):
+                if file2.endswith(".fq.gz") or file2.endswith(".fasta.gz"):
+                    print(f"{self.tool_path} -a {self.refseq} {filename}/{file2} > {self.output_path}/{file2.split('.')[0]}_aligned.sam")
+                    os.system(f"{self.tool_path} -a {self.refseq} {filename}/{file2} > {self.output_path}/{file2.split('.')[0]}_aligned.sam")
+
+       # data = glob.glob("*.fa") + glob.glob("*.fasta")
+        #data = ["fa", "fa"]
+        #for file in data:
+        #    print(f"{self.tool_path} -a {self.refseq} {file} > {self.output_path}/{file.split('.')[0]}_aligned.sam")
+        #    # os.system(f"{self.tool_path} -a {self.refseq} {file} > {file.split('.')[-1]}_aligned.sam")
+
+
+def construct_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--fastqDir', help='Directory to the fq.gz/fastq.gz files')
+    parser.add_argument('-o', '--organism',
+                        help='Define the two letter id for the organism for the '
+                             'alignment:\nHuman=hs\nMouse=mm\nMacaque=mmu\nRat=rn')
+    parser.add_argument('-out', '--outputDir', help='Pathways to output directory')
+    parser.add_argument('-s', '--seqType',
+                        help='Define SE for single end sequencing or PE for paired end sequencing')
+    parser.add_argument('-p', '--threads', help='Define number of threads to use', default=4, type=int)
+    parser.add_argument('-t', '--trim', help='Define the last bp to keep for trimming')
+    parser.add_argument('-S', '--skip', help='Skip already processed files', action="store_true", default=False)
+    parser.add_argument('-q', '--quality', help='Define cut-off value for trimming', type=int, default=20)
+    return parser
+
+
+def main():
+    parser = construct_parser()
+    args = parser.parse_args()
+    yes = Alignment(f"{args.outputDir}/output/trimmed_data", "/data/storix2/student/2020-2021/Thema10/tmp/tools/pipeline_tools/minimap2", "", f"{args.outputDir}/output/sam_files")
+    yes.processing()
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
