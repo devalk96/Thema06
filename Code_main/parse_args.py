8e5861d9977dfa878220757e4ebba79a9db16d73
a <cojar43405>
2021-01-13 10:22:58 +0100

    - Added default tools arguments. - Added test for tools. - Test Tools existence.

--- Code_main/parse_args.py
+++ Code_main/parse_args.py
@@ -16,10 +16,11 @@ def construct_parser():
     parser.add_argument('-q', '--quality', help='Define cut-off value for trimming', type=int, default=20)
     parser.add_argument('-r', '--refseq', help='Reference genome to align to', type=str, required=True)
     parser.add_argument('-g', '--gtf', help='Gtf file', type=str, required=True)
-    parser.add_argument('-l', '--trimgalore', help='Path to Trimgalore', type=str, required=True)
-    parser.add_argument('-c', '--cutadapt', help='Path to Cutadapt', type=str, required=True)
-    parser.add_argument('-m', '--minimap2', help='Path to minimap2', type=str, required=True)
-    parser.add_argument('-f', '--fastqc', help='Path to Fastqc', type=str, required=True)
-    parser.add_argument('-e', '--featurecounts', help='Path to Featurecounts', type=str, required=True)
+    parser.add_argument('-l', '--trimgalore', help='Path to Trimgalore', type=str, default='trim_galore')
+    parser.add_argument('-c', '--cutadapt', help='Path to Cutadapt', type=str, default='cutadapt')
+    parser.add_argument('-m', '--minimap2', help='Path to minimap2', type=str, default='minimap2')
+    parser.add_argument('-f', '--fastqc', help='Path to Fastqc', type=str, default='fastqc')
+    parser.add_argument('-e', '--featurecounts', help='Path to Featurecounts', type=str,  default='featureCounts')
+
 
     return parser
